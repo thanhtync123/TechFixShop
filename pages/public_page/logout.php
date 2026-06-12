f2b68eabf5b352ce1977bb4e65dccb51276a0e2a
Nguyễn Thành Tỷ <22004071@st.vlute.edu.vn>
2026-06-12 11:54:12 +0700

    delete project cũ

--- pages/public_page/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_GET['action']) && $_GET['action'] == 'logout') {
-    session_unset();
-    session_destroy();
-    header('Location: /TechFixPHP/pages/public_page/login.php');
-    exit;
-}
-?>
